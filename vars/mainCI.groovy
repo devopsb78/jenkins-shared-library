47e7184e9210486e15d91005f84b2522682d49b6
RAMBABU <chinnak7416@gmail.com>
2024-08-12 21:37:52 +0530

    Adding code checkout stage

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -1,11 +1,28 @@
 def call() {
     node('ci-server') {
+
+        stage('CodeCheckout') {
+
+            sh "find ."
+            sh "find . | sed -e '1d' |xargs rm -rf"
+            if(env.TAG_NAME ==~ ".*") {
+                env.branch_name = "refs/tags/${env.TAG_NAME}"
+            } else {
+                env.branch_name = "${env.BRANCH_NAME}"
+            }
+            checkout scmGit(
+                    branches: [[name: "${branch_name}"]],
+                    userRemoteConfigs: [[url: "https://github.com/raghudevopsb78/roboshop-${component}"]]
+            )
+        }
+
         if (env.TAG_NAME ==~ '.*') {
             stage('Build Code') {
-                print 'OK'
+                sh 'docker build -t 368761340104.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
             }
             stage('Release Software') {
-                print 'OK'
+                sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 368761340104.dkr.ecr.us-east-1.amazonaws.com'
+                sh 'docker push -t 368761340104.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME}'
             }
         }
         else {
